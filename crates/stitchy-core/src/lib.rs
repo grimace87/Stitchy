ed5e801efd86c0a699e1a93090c7ea645db6b0b7
Thomas Reichert <thomasr1987@gmail.com>
2023-04-26 06:46:11 +1000

    Crate-level comment for stitchy-core

--- crates/stitchy-core/src/lib.rs
+++ crates/stitchy-core/src/lib.rs
@@ -1,3 +1,35 @@
+//! Joining images together into a single image.
+//!
+//! Provides two processes, each configured using the builder pattern:
+//! - Selecting image files
+//! - Copying the (typically resized) contents of those files into a single output image
+//!
+//! The first process is performed by the [`ImageFiles`] struct and its associated builder pattern.
+//! Files are added either individually or from a directory.
+//!
+//! The second process is performed by the [`Stitch`] struct and its associated builder pattern.
+//! The configuration sets the output image size and layout of the output image. The output image
+//! is returned as an in-memory struct of type [`DynamicImage`], re-exported from the image crate
+//! (see the [image crate on crates.io](https://crates.io/crates/image)).
+//!
+//! # Examples
+//!
+//! ```
+//! // Select image files in current directory
+//! use stitchy_core::ImageFiles;
+//! let image_files = ImageFiles::builder()
+//!     .add_current_directory(vec![]).unwrap()
+//!     .build().unwrap();
+//!
+//! // Stitch images in a horizontal line, restricting the width to 1000 pixels
+//! use stitchy_core::{Stitch, AlignmentMode};
+//! let image = Stitch::builder()
+//!     .image_files(image_files)
+//!     .width_limit(1000)
+//!     .alignment(AlignmentMode::Horizontal)
+//!     .stitch().unwrap();
+//! ```
+
 mod enums;
 mod files;
 mod stitch;
